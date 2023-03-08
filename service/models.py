406b215e880754969ffdb174825d1f80b9787930
taliareg <taliaregenstein@gmail.com>
2023-03-08 19:29:39 +0000

    -m commit

--- service/models.py
+++ service/models.py
@@ -1,9 +1,12 @@
 """
-Models for YourResourceModel
+Models for Wishlists
 
 All of the models are stored in this module
 """
+
 import logging
+from datetime import date
+from abc import abstractmethod
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -12,19 +15,83 @@ logger = logging.getLogger("flask.app")
 db = SQLAlchemy()
 
 
-# Function to initialize the database
-def init_db(app):
-    """ Initializes the SQLAlchemy app """
-    YourResourceModel.init_db(app)
-
-
 class DataValidationError(Exception):
-    """ Used for an data validation errors when deserializing """
+    """Used for an data validation errors when deserializing"""
 
 
-class YourResourceModel(db.Model):
+def init_db(app):
+    """Initialize the SQLAlchemy app"""
+    Account.init_db(app)
+
+
+######################################################################
+#  P E R S I S T E N T   B A S E   M O D E L
+######################################################################
+class PersistentBase:
+    """Base class added persistent methods"""
+
+    def __init__(self):
+        self.id = None  # pylint: disable=invalid-name
+
+    @abstractmethod
+    def serialize(self) -> dict:
+        """Convert an object into a dictionary"""
+
+    @abstractmethod
+    def deserialize(self, data: dict) -> None:
+        """Convert a dictionary into an object"""
+
+    def create(self):
+        """
+        Creates a Account to the database
+        """
+        logger.info("Creating %s", self.name)
+        self.id = None  # id must be none to generate next primary key
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates a Account to the database
+        """
+        logger.info("Updating %s", self.name)
+        db.session.commit()
+
+    def delete(self):
+        """Removes a Account from the data store"""
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def init_db(cls, app):
+        """Initializes the database session"""
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls):
+        """Returns all of the records in the database"""
+        logger.info("Processing all records")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, by_id):
+        """Finds a record by it's ID"""
+        logger.info("Processing lookup for id %s ...", by_id)
+        return cls.query.get(by_id)
+
+
+######################################################################
+#  WISHLIST MODEL
+######################################################################
+class Wishlist(db.Model, PersistantBase):
     """
-    Class that represents a YourResourceModel
+    Class that represents a wishlist
     """
 
     app = None
@@ -33,14 +100,14 @@ class YourResourceModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(63), nullable=False)
     customer_id = db.Column(db.String(63), nullable=False)
-    item_id = db.Column(db.String(63), nullable=True) #can be an empty list
+
 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
+        return f"<Wishlist {self.name} id=[{self.id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Wishlist to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # pylint: disable=invalid-name
@@ -49,24 +116,24 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Wishlist to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a Wishlist from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name, "customer id": self.customer_id, "item id": self.item_id}
+        """ Serializes a Wishlist into a dictionary """
+        return {"id": self.id, "name": self.name, "customer id": self.customer_id}
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Wishlist from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
@@ -75,15 +142,84 @@ class YourResourceModel(db.Model):
             self.name = data["name"]
             self.id = data['id']
             self.customer_id = data['customer id']
-            if data['item_id'] != None:
-                self.item_id = data['item id']
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
+                "Invalid Wishlist: missing " + error.args[0]
             ) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
+                "Invalid Wishlist: body of request contained bad or no data - "
+                "Error message: " + error
+            ) from error
+        return self
+
+
+######################################################################
+#  ITEMS MODEL
+######################################################################
+class item(db.Model, PersistantBase):
+    """
+    Class that represents a item
+    """
+
+    app = None
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63), nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+    wishlist_id = db.relationship("Wishlist", backref="items", passive_deletes=True)
+    item_id = db.Column(db.String(63), nullable=True) #can be an empty list
+
+    def __repr__(self):
+        return f"<Item {self.name} id=[{self.id}]>"
+
+    def create(self):
+        """
+        Creates a item to the database
+        """
+        logger.info("Creating %s", self.name)
+        self.id = None  # pylint: disable=invalid-name
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates a Item to the database
+        """
+        logger.info("Saving %s", self.name)
+        db.session.commit()
+
+    def delete(self):
+        """ Removes a Item from the data store """
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """ Serializes a Item into a dictionary """
+        return {"id": self.id, "name": self.name, "wishlist id": self.wishlist_id, "count": self.count, "item id": self.item_id}
+
+    def deserialize(self, data):
+        """
+        Deserializes a Item from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.name = data["name"]
+            self.id = data['id']
+            self.item_id = data['item_id']
+            self.wishlist_id = data['wishlist_id']
+            self.count = data['count']
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Items: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Items: body of request contained bad or no data - "
                 "Error message: " + error
             ) from error
         return self
