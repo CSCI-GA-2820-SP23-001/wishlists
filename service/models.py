e96075988e4d08c1cc3bac963ad143a4f520f3de
taliareg <taliaregenstein@gmail.com>
2023-03-09 14:49:22 +0000

    -m update

--- service/models.py
+++ service/models.py
@@ -3,7 +3,6 @@ Models for Wishlists
 
 All of the models are stored in this module
 """
-
 import logging
 from datetime import date
 from abc import abstractmethod
@@ -21,7 +20,7 @@ class DataValidationError(Exception):
 
 def init_db(app):
     """Initialize the SQLAlchemy app"""
-    Account.init_db(app)
+    Wishlist.init_db(app)
 
 
 ######################################################################
@@ -87,171 +86,120 @@ class PersistentBase:
 
 
 ######################################################################
-#  WISHLIST MODEL
+#  I T E M   M O D E L
 ######################################################################
-class Wishlist(db.Model, PersistantBase):
+class Item(db.Model, PersistentBase):
     """
-    Class that represents a wishlist
+    Class that represents an Item
+    """
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    wishlist_id = db.Column(db.Integer, db.ForeignKey("wishlist.id", ondelete="CASCADE"), nullable=False)
+    item_id = db.Column(db.Integer(64))
+    count = db.Column(db.Integer(64))
+
+
+    def __repr__(self):
+        return f"<Item {self.item_id} id=[{self.id}] wishlist[{self.wishlist_id}]>"
+
+    def __str__(self):
+        return f"{self.id}: {self.item_id}, {self.wishlist_id}, {self.count}"
+
+    def serialize(self) -> dict:
+        """Converts an Item into a dictionary"""
+        return {
+            "id": self.id,
+            "wishlist_id": self.wishlist_id,
+            "item_id": self.item_id,
+            "count": self.count
+        }
+
+    def deserialize(self, data: dict) -> None:
+        """
+        Populates an Address from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.id = data["id"]
+            self.wishlist_id = data["wishlist_id"]
+            self.item_id = data["item_id"]
+            self.count = data["count"]
+        except KeyError as error:
+            raise DataValidationError("Invalid Wishlist: missing " + error.args[0]) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Wishlist: body of request contained "
+                "bad or no data " + error.args[0]
+            ) from error
+        return self
+
+
+######################################################################
+#  W I S H L I S T   M O D E L
+######################################################################
+class Wishlist(db.Model, PersistentBase):
+    """
+    Class that represents an Wishlist
     """
 
     app = None
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63), nullable=False)
-    customer_id = db.Column(db.String(63), nullable=False)
-
+    account_id = db.Column(db.Integer)
+    name = db.Column(db.String(64))
+    items = db.relationship("Item", backref="wishlist", passive_deletes=True)
 
     def __repr__(self):
-        return f"<Wishlist {self.name} id=[{self.id}]>"
-
-    def create(self):
-        """
-        Creates a Wishlist to the database
-        """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        Updates a Wishlist to the database
-        """
-        logger.info("Saving %s", self.name)
-        db.session.commit()
-
-    def delete(self):
-        """ Removes a Wishlist from the data store """
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
+        return f"<Wishlists {self.name} id=[{self.id}]>"
 
     def serialize(self):
-        """ Serializes a Wishlist into a dictionary """
-        return {"id": self.id, "name": self.name, "customer id": self.customer_id}
+        """Converts an Wishlist into a dictionary"""
+        wishlist = {
+            "id": self.id,
+            "account": self.account_id,
+            "name": self.name,
+            "items": [],
+        }
+        for item in self.items:
+            wishlist["items"].append(items.serialize())
+        return wishlist
 
     def deserialize(self, data):
         """
-        Deserializes a Wishlist from a dictionary
+        Populates an Account from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
+            self.id = data["id"]
+            self.account = data["account_id"]
             self.name = data["name"]
-            self.id = data['id']
-            self.customer_id = data['customer id']
+            # handle inner list of addresses
+            items_list = data.get("items")
+            for json_items in items_list:
+                item = Item()
+                item.deserialize(json_item)
+                self.items.append(item)
         except KeyError as error:
-            raise DataValidationError(
-                "Invalid Wishlist: missing " + error.args[0]
-            ) from error
+            raise DataValidationError("Invalid Wishlist: missing " + error.args[0]) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid Wishlist: body of request contained bad or no data - "
-                "Error message: " + error
+                "Invalid Wishlist: body of request contained "
+                "bad or no data - " + error.args[0]
             ) from error
         return self
-
-
-######################################################################
-#  ITEMS MODEL
-######################################################################
-class item(db.Model, PersistantBase):
-    """
-    Class that represents a item
-    """
-
-    app = None
-
-    # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63), nullable=False)
-    count = db.Column(db.Integer, nullable=False)
-    wishlist_id = db.relationship("Wishlist", backref="items", passive_deletes=True)
-    item_id = db.Column(db.String(63), nullable=True) #can be an empty list
-
-    def __repr__(self):
-        return f"<Item {self.name} id=[{self.id}]>"
-
-    def create(self):
-        """
-        Creates a item to the database
-        """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        Updates a Item to the database
-        """
-        logger.info("Saving %s", self.name)
-        db.session.commit()
-
-    def delete(self):
-        """ Removes a Item from the data store """
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
-
-    def serialize(self):
-        """ Serializes a Item into a dictionary """
-        return {"id": self.id, "name": self.name, "wishlist id": self.wishlist_id, "count": self.count, "item id": self.item_id}
-
-    def deserialize(self, data):
-        """
-        Deserializes a Item from a dictionary
-
-        Args:
-            data (dict): A dictionary containing the resource data
-        """
-        try:
-            self.name = data["name"]
-            self.id = data['id']
-            self.item_id = data['item_id']
-            self.wishlist_id = data['wishlist_id']
-            self.count = data['count']
-        except KeyError as error:
-            raise DataValidationError(
-                "Invalid Items: missing " + error.args[0]
-            ) from error
-        except TypeError as error:
-            raise DataValidationError(
-                "Invalid Items: body of request contained bad or no data - "
-                "Error message: " + error
-            ) from error
-        return self
-
-    @classmethod
-    def init_db(cls, app):
-        """ Initializes the database session """
-        logger.info("Initializing database")
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
-
-    @classmethod
-    def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
-        return cls.query.all()
-
-    @classmethod
-    def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all Wishlists with the given name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            name (string): the name of the Wishlists you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
