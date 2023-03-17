b451a166e2a9458fa5927885ffcf67f132229f3b
taliareg <taliaregenstein@gmail.com>
2023-03-17 02:48:35 +0000

    updated create function

--- service/routes.py
+++ service/routes.py
@@ -6,7 +6,8 @@ Describe what your service does here
 
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from service.common import status  # HTTP Status Codes
-from service.models import PersistentBase
+from service.models import PersistentBase, Wishlist, Item
+from . import app  # Import Flask application
 
 # Import Flask application
 from . import app
@@ -24,8 +25,51 @@ def index():
     )
 
 
+
+######################################################################
+# CREATE A NEW WISHLIST
+######################################################################
+@app.route("/wishlists", methods=["POST"])
+def create_wishlists():
+    """
+    Creates a wishlist
+    This endpoint will create an wishlist based the data in the body that is posted
+    """
+    app.logger.info("Request to create an wishlist")
+    check_content_type("application/json")
+
+    # Create the wishlist
+    wishlist = Wishlist()
+    wishlist.deserialize(request.get_json())
+    wishlist.create()
+
+    # Create a message to return
+    message = wishlist.serialize()
+    location_url = url_for("create_wishlists", wishlist_id=wishlist.id, _external=True)
+
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+
+
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
 
 # Place your REST API code here ...
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {media_type}",
+    )
