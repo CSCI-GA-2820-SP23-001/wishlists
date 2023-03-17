5711954b07d7fc33689d2967ef2e1d35f3e8a2da
taliareg <taliaregenstein@gmail.com>
2023-03-17 02:52:33 +0000

    update route

--- service/routes.py
+++ service/routes.py
@@ -45,8 +45,10 @@ def create_wishlists():
 
     # Create a message to return
     message = wishlist.serialize()
-    location_url = url_for("create_wishlists", wishlist_id=wishlist.id, _external=True)
-
+    # Uncomment this code once get_wishlists is implemented
+    # location_url = url_for("get_wishlists", wishlist_id=wishlist.id, _external=True)
+    location_url = "Unknown"
+    
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
