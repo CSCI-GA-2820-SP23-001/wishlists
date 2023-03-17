e01c98c2d30b3933229f52ff0b502ae7cc12e278
taliareg <taliaregenstein@gmail.com>
2023-03-17 16:13:45 +0000

    added list wishlist route/test

--- service/routes.py
+++ service/routes.py
@@ -6,7 +6,8 @@ Describe what your service does here
 
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from service.common import status  # HTTP Status Codes
-from service.models import PersistentBase
+from service.models import PersistentBase, Wishlist, Item
+from . import app  # Import Flask application
 
 # Import Flask application
 from . import app
@@ -25,7 +26,72 @@ def index():
 
 
 ######################################################################
+# CREATE A NEW WISHLIST
+######################################################################
+@app.route("/wishlists", methods=["POST"])
+def create_wishlists():
+    """
+    Creates a wishlist
+    This endpoint will create an wishlist based the data in the body that is posted
+    """
+    app.logger.info("Request to create an wishlist")
+    check_content_type("application/json")
+
+    # Create the wishlist
+    wishlist = Wishlist()
+    wishlist.deserialize(request.get_json())
+    wishlist.create()
+
+    # Create a message to return
+    message = wishlist.serialize()
+    # Uncomment this code once get_wishlists is implemented
+    # location_url = url_for("get_wishlists", wishlist_id=wishlist.id, _external=True)
+    location_url = "Unknown"
+    
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+
+
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
+@app.route("/wishlists", methods=["GET"])
+def list_wishlists():
+    """Returns all of the Wishlists"""
+    app.logger.info("Request for Wishlist list")
+    wishlists = []
+
+    # Process the query string if any
+    name = request.args.get("name")
+    if name:
+        wishlists = Wishlist.find_by_name(name)
+    else:
+        wishlists = Wishlist.all()
+
+    # Return as an array of dictionaries
+    results = [wishlist.serialize() for wishlist in wishlists]
+
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+######################################################################
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
 
 # Place your REST API code here ...
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {media_type}",
+    )
