e653b4d90f04510945352964ac827bdccfb62229
NealRyan <78014900+NealRyan@users.noreply.github.com>
2023-04-30 20:45:47 -0400

    adding item ui
    added a item ui for CRUD

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -4,6 +4,10 @@ $(function () {
     //  U T I L I T Y   F U N C T I O N S
     // ****************************************
 
+    // ****************************************
+    //  WISHLISTS
+    // ****************************************
+
     // Updates the form with data from the response
     function update_form_data(res) {
         $("#wishlist_name").val(res.wishlist_name);
@@ -30,7 +34,7 @@ $(function () {
     // Create a Wishlist
     // ****************************************
 
-    $("#create-btn").click(function () {
+    $("#wishlist-create-btn").click(function () {
         let id = $("#wishlist_id").val();
         let name = $("#wishlist_name").val();
         let account_id = $("#account_id").val();
@@ -68,7 +72,7 @@ $(function () {
     // Update a wishlist
     // ****************************************
 
-    $("#update-btn").click(function () {
+    $("#wishlist-update-btn").click(function () {
         let name = $("#wishlist_name").val();
         let id = $("#wishlist_id").val();
         let account_id = $("#account_id").val();
@@ -135,7 +139,7 @@ $(function () {
     // Delete a Wishlist
     // ****************************************
 
-    $("#delete-btn").click(function () {
+    $("#wishlist-delete-btn").click(function () {
 
         let wishlist_id = $("#wishlist_id").val();
 
@@ -172,7 +176,7 @@ $(function () {
     // Search for a Wishlist
     // ****************************************
 
-    $("#search-btn").click(function () {
+    $("#wishlist-search-btn").click(function () {
 
         let wishlist_name = $("#wishlist_name").val();
 
@@ -232,4 +236,229 @@ $(function () {
 
     });
 
+    // ****************************************
+    //  ITEMS
+    // ****************************************
+
+    // ****************************************
+    // Add Item to a Wishlist
+    // ****************************************
+
+    $("#create-item-btn").click(function () {
+
+        let item_id = $("#item_id").val();
+        let wishlist_id = $("#wishlist_id_2").val();
+        let sku = $("#sku").val();
+        let item_available = $("#item_available").val();
+        let item_count;
+        if( ($("#item_count").val()) == ""){
+            item_count = 1;
+        } else{
+            item_count = $("#item_count").val();
+        }
+        
+        let data = {
+            "id": item_id,
+            "wishlist_id": wishlist_id,
+            "sku": parseInt(product_id),
+            "item_available": item_available,
+            "count": parseInt(item_count),
+        };
+
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "POST",
+            url: `/api/wishlists/${wishlist_id}/items`,
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data_item(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+    // ****************************************
+    // Retrieve an Item
+    // ****************************************
+
+    $("#retrieve-item-btn").click(function () {
+
+        let item_id = $("#item_id").val();
+        // doesn't matter
+        let wishlist_id = 1;
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/api/wishlists/${item_id}/items/${item_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data_item(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete an Item
+    // ****************************************
+
+    $("#delete-item-btn").click(function () {
+
+        let item_id = $("#item_id").val();
+        // doesn't matter
+        let wishlist_id = 1;
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/api/wishlists/${item_id}/items/${item_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("Item has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Update a Wishlist Item
+    // ****************************************
+
+    $("#update-item-btn").click(function () {
+
+        let id = $("#item_id").val();
+        let wishlist_id = $("#wishlist_id_2").val();
+        let sku = $("#sku").val();
+        let item_available = $("#item_available").val();
+        let count = $("#item_count").val();
+   
+        let data = {
+            "id": item_id,
+            "wishlist_id": wishlist_id,
+            "sku": parseInt(sku),
+            "item_available": item_available,
+            "count": parseInt(count),
+        };
+
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/api/wishlists/${wishlist_id}/items/${item_id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data_item(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+
+    // ****************************************
+    // Search for a Wishlist Item
+    // ****************************************
+
+    $("#search-item-btn").click(function () {
+
+        let item_id = $("#item_id").val();
+        let wishlist_id = $("#wishlist_id_2").val();
+
+        let queryString = ""
+
+        if(item_id){
+            get_url = `/api/wishlists/${item_id}/items/${item_id}`
+        } else if (item_id){
+            queryString += 'name=' + item_name
+            get_url = `/api/wishlists/${wishlist_id}/items?${queryString}`
+        } else{
+            get_url = `/api/wishlists/${wishlist_id}/items`
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: get_url,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_item_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-1">Item ID</th>'
+            table += '<th class="col-md-3">Wishlist ID</th>'
+            table += '<th class="col-md-4">SKU</th>'
+            table += '<th class="col-md-2">Item Available</th>'
+            table += '<th class="col-md-2">Count</th>'
+            table += '</tr></thead><tbody>'
+            let firstIList = "";
+            if(item_id){
+                let item = res;
+                table +=  `<tr id="row_0"><td>${item.id}</td><td>${item.wishlist_id}</td><td>${item.sku}</td><td>${item.item_available}</td>
+                <td>${item.count}</td></tr>`;
+                firstIList = item;
+            } else{
+                for(let i = 0; i < res.length; i++) {
+                    let item = res[i];
+                    table +=  `<tr id="row_${i}"><td>${item.id}</td><td>${item.wishlist_id}</td><td>${item.sku}</td><td>${item.item_available}</td>
+                    <td>${item.count}</td></tr>`;
+                    if (i == 0) {
+                        firstIList = item;
+                    }
+                }
+            }
+            
+
+            table += '</tbody></table>';
+            $("#search_item_results").append(table);
+
+            // copy the first result to the form
+            if (firstIList != "") {
+                update_form_data_item(firstIList)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
 })
